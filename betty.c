41659543ec612828aa1bbfbea70956fa205c701d
Mulimbwe <mulimbwensefu@gmail.com>
2022-09-21 10:19:59 -0700

    main.c

---
+++ betty.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+void main(void)
+{
+	int test = 5;
+
+	printf("%d\n",	test);
+}
+
+# 942 "/usr/include/stdio.h" 3 4
+
+# 2 "main.c" 2
+
+
+# 3 "main.c"
+
+int main(void)
+
+{
+
+	 return (0);
+
+}
+
